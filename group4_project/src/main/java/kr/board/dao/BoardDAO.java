bfc006232018333ec055d576aeb9c7ab4c5318ff
narae9 <rnskfo55@naver.com>
2022-07-20 16:24:26 +0900

    게시판 vo, dao 기초틀

---
+++ group4_project/src/main/java/kr/board/dao/BoardDAO.java
@@ -0,0 +1,38 @@
+package kr.board.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.board.vo.BoardVO;
+import kr.util.DBUtil;
+
+public class BoardDAO {
+	//싱글턴 패턴
+	private static BoardDAO instance = new BoardDAO();
+	
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	private BoardDAO() {}
+	
+	//글등록(관리자 임의 작성)
+	//총 레코드 수(검색 레코드 수)
+	//글목록(검색글 목록)
+	//글상세
+	
+	//댓글 등록
+	//댓글 개수
+	//댓글 목록
+	//댓글 상세
+	//댓글 수정
+	//댓글 삭제
+	
+}
+
+
+
+
+
---
+++ group4_project/src/main/java/kr/board/vo/BoardVO.java
@@ -0,0 +1,85 @@
+package kr.board.vo;
+
+import java.sql.Date;
+
+//자바빈(VO)
+public class BoardVO {
+	private int me_key;
+	private int bo_key;
+	private int bom_key;
+	private String bo_title;
+	private String bo_write;
+	private String bom_write;
+	private Date bo_reg_date;
+	private Date bom_reg_date;
+	private Date bo_mod_date;
+	private Date bom_mod_date;
+	
+	
+	public int getMe_key() {
+		return me_key;
+	}
+	public void setMe_key(int me_key) {
+		this.me_key = me_key;
+	}
+	public int getBo_key() {
+		return bo_key;
+	}
+	public void setBo_key(int bo_key) {
+		this.bo_key = bo_key;
+	}
+	public int getBom_key() {
+		return bom_key;
+	}
+	public void setBom_key(int bom_key) {
+		this.bom_key = bom_key;
+	}
+	public String getBo_title() {
+		return bo_title;
+	}
+	public void setBo_title(String bo_title) {
+		this.bo_title = bo_title;
+	}
+	public String getBo_write() {
+		return bo_write;
+	}
+	public void setBo_write(String bo_write) {
+		this.bo_write = bo_write;
+	}
+	public String getBom_write() {
+		return bom_write;
+	}
+	public void setBom_write(String bom_write) {
+		this.bom_write = bom_write;
+	}
+	public Date getBo_reg_date() {
+		return bo_reg_date;
+	}
+	public void setBo_reg_date(Date bo_reg_date) {
+		this.bo_reg_date = bo_reg_date;
+	}
+	public Date getBom_reg_date() {
+		return bom_reg_date;
+	}
+	public void setBom_reg_date(Date bom_reg_date) {
+		this.bom_reg_date = bom_reg_date;
+	}
+	public Date getBo_mod_date() {
+		return bo_mod_date;
+	}
+	public void setBo_mod_date(Date bo_mod_date) {
+		this.bo_mod_date = bo_mod_date;
+	}
+	public Date getBom_mod_date() {
+		return bom_mod_date;
+	}
+	public void setBom_mod_date(Date bom_mod_date) {
+		this.bom_mod_date = bom_mod_date;
+	}
+}
+
+
+
+
+
+
