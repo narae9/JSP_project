46a05b21a22f2ad93c2f55b86722e53f056cdcaf
narae9 <rnskfo55@naver.com>
2022-07-27 11:39:09 +0900

    main스르륵

--- group4_project/src/main/webapp/js/main.js
+++ group4_project/src/main/webapp/js/main.js
@@ -1,5 +1,10 @@
-$(function() {
-    $("#test1").on("click", function() {
-        $(this).attr("id", "#test2");
-    });
+$(function(){
+	$('#test1').click(function(){
+		document.getElementById("test1").style.display = "none";
+		document.getElementById("test5").style.display = "none";
+		document.getElementById("test4").style.display = "block";
+		  $('html, body').animate({
+  			scrollTop: $(".page-main").offset().top 
+ 			}, 'slow');
+	});
 });
