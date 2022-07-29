0e0715d2c1615f3b20004695d059b5425aeb4584
user <user@DESKTOP-COJ9858>
2022-07-29 17:03:17 +0900

    js진행중

--- group4_project/src/main/webapp/js/show.js
+++ group4_project/src/main/webapp/js/show.js
@@ -1,26 +1,35 @@
+$(function(){
 
-//이미지 전송
-$('#InsertSubmit_btn').click(function(){
-	
-	//파일 전송
-	let form_data = new FormData(); //여기에 파일저장을해줌
-				  //(파라미터네임, 벨류)
-	$.ajax({
-		url:'updatePhoto.do',
-		type:'post',
-		data:form_data,
-		dataType:'json',
-		contentType:false,//데이터 객체를 문자열로 바꿀지에 대한 값 true면 일반문자
-		processData:false,//해당 타입을 true라고 명시하면 일반 text로 인식 -> 현재는 파일이라는것
-		enctype:'multipart/form-data',
-		success:function(param){
-			
-		},
-		error:function(){
-			alert('네트워크 오류 발생');
+
+	//===================공연 등록 & 공연 수정=====================//
+	$('#showInsertForm').submit(function(){
+		if($('#sh_title').val().trim()==''){
+			alert('제목을 입력하세요');
+			$('#sh_title').val('').focus();
+			return false;	
 		}
-		
+		if($('#sh_date').val().trim()==""){
+			alert('날짜를 입력하세요');
+			$('#sh_date').val('').focus();
+			return false;	
+		}
+		if($('#sh_time').val().trim()==""){
+			alert('시간을 입력하세요');
+			$('#sh_time').val('').focus();
+			return false;	
+		}
+		if($('#sh_place').val().trim()==""){
+			alert('장소를 입력하세요');
+			$('#sh_place').val('').focus();
+			return false;	
+		}
+		if($('#sh_detail').val().trim()==""){
+			alert('공연 설명을 입력하세요');
+			$('#sh_detail').val('').focus();
+			return false;	
+		}
 	});
 	
-});//end of click
 
+
+});
